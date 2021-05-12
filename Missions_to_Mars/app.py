aca3c1de290e190dfcfe9969def012cc36379b09
michellesimek <76979376+michellesimek@users.noreply.github.com>
2021-05-12 18:29:04 -0500

    fixed database issue to information now correctly shows in scrape

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -9,19 +9,17 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
 def home():
-    mars = mongo.db.collection.find_one()
+    mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
 
 
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-
     # Run the scrape function
     mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mars.update({}, mars_data, upsert=True)
+    mongo.db.mars.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
