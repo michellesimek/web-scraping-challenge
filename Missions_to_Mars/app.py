f155d6a35288322f0b80abce0f0d57db4be7e3c2
michellesimek <76979376+michellesimek@users.noreply.github.com>
2021-05-12 08:26:19 -0500

    split up one big function into smaller functions

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -9,18 +9,19 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
 def home():
-    planet_data = mongo.db.collection.find_one()
-    return render_template("index.html", mars=planet_data)
+    mars = mongo.db.collection.find_one()
+    return render_template("index.html", mars=mars)
 
 
 @app.route("/scrape")
 def scrape():
+    mars = mongo.db.mars
 
     # Run the scrape function
-    data = scrape_mars.scrape()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, data, upsert=True)
+    mars.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
